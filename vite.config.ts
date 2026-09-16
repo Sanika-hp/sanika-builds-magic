bc7f06c6e681ce95bb323d33c9188f2bf610f603
SANIKA H P <sanikahp4@gmail.com>
2026-09-16 18:19:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: "/sanika-builds-magic/",
+  base: "./",
   tanstackStart: {
     server: { entry: "server" },
     prerender: {
